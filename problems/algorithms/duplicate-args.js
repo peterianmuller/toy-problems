2eee0f51ae1fbf4b09bd34b2c071e36a434ece21
peterianmuller <peterianmuller@gmail.com>
2019-07-10 16:18:39 -0700

    Add pseudocode

--- problems/algorithms/duplicate-args.js
+++ problems/algorithms/duplicate-args.js
@@ -1,4 +1,11 @@
 // I -> varied number of arguments
 // O -> true if there are any duplicates in args list, false otherwise
 
+// iterate over args
+// declare freq set to new Set
+// for each arg
+// if freq has a property at arg return true
+// otherwise add arg to set
+// return false
+
 module.exports = (...arguments) => {};
